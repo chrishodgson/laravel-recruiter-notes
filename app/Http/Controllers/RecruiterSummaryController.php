a05ad90c1d365a20fb8da8adcd33f29183fa332b
Chris Hodgson <chris.3.hodgson@plus.net>
2019-10-07 15:57:31 +0100

    add back in sub query

--- app/Http/Controllers/RecruiterSummaryController.php
+++ app/Http/Controllers/RecruiterSummaryController.php
@@ -2,11 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Note;
 use App\Recruiter;
 use Illuminate\Http\Response;
 
 class RecruiterSummaryController extends Controller
 {
+    /**
+     * https://laravel.com/docs/6.x/queries#joins - Sub-Query Joins
+     * get recruiter ordered by latest note at and do a Sub-Query Join on the latest note
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index2()
+    {
+        $latestNoteQuery = Note::select(
+            'recruiter_id', 'updated_at', 'details as latest_note_details', 'follow_up as latest_note_follow_up'
+        )->orderBy('updated_at', 'desc')->take(1);
+
+        $recruiters = Recruiter::joinSub($latestNoteQuery, 'latest_note', function ($join) {
+            $join->on('recruiters.id', '=', 'latest_note.recruiter_id');
+        })->orderBy('latest_note_at', 'desc')
+          ->simplePaginate(10);
+
+        return view('summary.index', compact('recruiters'));
+    }
+
     /**
      * Display a listing of the resource.
      *
